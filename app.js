2e118c0a749661ac2c9a8c16c3228705f83a3786
Devin Checa <devincheca@gmail.com>
2018-01-01 14:34:45 -0800

    static files are served properly

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'reactOutput')));
+app.use(express.static('public'));
+app.use(express.static('reactOutput'));
 
 app.use('/', index);
 app.use('/users', users);
