e5ac6ea79477c05291e3d8ea4ebeac038b18d96d
Devin Checa <devincheca@gmail.com>
2018-01-18 14:29:54 -0800

    began implementing chart.js

--- reactDev/components/Chart.jsx
+++ reactDev/components/Chart.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from '../styles.jsx'
 
+
+
 const Chart = ({ source }) => 
 {
   return (
-    <img className="img-fluid animated slideInDown" style={styles.orchid.orchidBorder} src={source.location} />
+    <div className="animated slideInDown" style={styles.orchid.orchidBorder} src={source.location}>
+      
+    </div>
   )
+  /*return (
+    <img className="img-fluid animated slideInDown" style={styles.orchid.orchidBorder} src={source.location} />
+  )*/
 }
 
 Chart.propTypes = 
